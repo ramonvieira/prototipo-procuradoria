c787bd833f4b0c4f25f1a65efeee97f56e0dbf8d
Ramon Vieira Viquetti <ramonvieira@users.noreply.github.com>
2025-02-14 19:02:28 +0000

    Restored to '1d267c89e5f3e47d058f091fb8afa396bd5b8718'
    Replit-Restored-To:1d267c89e5f3e47d058f091fb8afa396bd5b8718

--- gerenciador-documentos/peticoes/peticoes.ctrl.js
+++ gerenciador-documentos/peticoes/peticoes.ctrl.js
@@ -4,23 +4,8 @@
 	.controller('DocsPeticoesCtrl', ['$scope', 'arrecadacao.common.ModalCad', 'peticoesService', '$state', '$stateParams',
 		function($scope, ModalCad, peticoesService, $state, $stateParams) {
 			var vm = $scope;
-            vm.peticoes = peticoesService.peticoes;
-            vm.selectedCount = 0;
-
-            vm.checkAll = function() {
-                var isChecked = vm.allChecked;
-                vm.peticoes.forEach(function(peticao) {
-                    peticao.selected = isChecked;
-                });
-                vm.selectedCount = isChecked ? vm.peticoes.length : 0;
-            };
-
-            vm.updateSelectedCount = function() {
-                vm.selectedCount = vm.peticoes.filter(function(peticao) {
-                    return peticao.selected;
-                }).length;
-            };
-
+            vm.peticoes = peticoesService.peticoes
+            
 			vm.tipo = $state.current.url.split('-')[1]
 
 			vm.executar = function(resolve) {
